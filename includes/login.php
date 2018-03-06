6831769bf9f9681d90f5868c429408007e776040
Praveen Kumar Guda <praveenguda522@gmail.com>
2018-03-06 14:46:31 +0100

    message changes

--- includes/login.php
+++ includes/login.php
@@ -24,7 +24,7 @@
         
         
         if(!$login_result){
-            die("Query Failed". mysqli_error($connection));
+            die("Query Failed for login". mysqli_error($connection));
         }
         if(mysqli_num_rows($login_result) == 1){
             while($row = mysqli_fetch_assoc($login_result)){
